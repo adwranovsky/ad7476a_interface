6738ef61dd768b00b6d233c34a287914f8b8cdd7
Alexander Wranovsky <alex@wranovsky.org>
2021-02-15 22:10:16 -0600

    Made some progress on the state transitions and instantiated clkdiv module

--- rtl/ad7476a_interface.v
+++ rtl/ad7476a_interface.v
@@ -14,7 +14,7 @@ module ad7476a_interface #(
 
     // SPI interface
     output wire sclk_o,
-    output wire cs_n_o,
+    output reg  cs_n_o,
     input  wire sdata_i
 );
     // Try to force an elaboration failure if invalid parameters were specified
@@ -51,32 +51,64 @@ module ad7476a_interface #(
         .done_o(timer_done)
     );
 
+    /*
+     * Create a clock divider module for generating sclk_o
+     */
+    reg enable_sclk;
+    clkdiv #(
+        DIV = CLK_DIV,
+        IDLE_HIGH = 1
+    ) sclk_generator (
+        .clk_i(clk_i),
+        .enable_i(enable_sclk),
+        .clk_o(sclk_o)
+    );
+
+    /*
+     * Detect rising and falling edges of sclk_o
+     */
+    reg last_sclk = 1;
+    always @(posedge clk_i)
+        last_sclk <= sclk_o
+    wire sclk_rising_edge  = sclk_o && !last_sclk;
+    wire sclk_falling_edge = !sclk_o && last_sclk;
+
+    /*
+     * Count 16 sclk_o falling edges
+     */
+    reg [2:0] sclk_falling_edge_counter = 0;
+    reg got_16_sclk_falling_edges = 0;
+    always @(posedge clk_i)
+        if (rst_i)
+            {got_16_sclk_falling_edges, num_sclk_falling_edges} <= 0;
+        else
+            {got_16_sclk_falling_edges, num_sclk_falling_edges} <= num_sclk_falling_edges + sclk_falling_edge;
 
     /*
      * Create a shift register for reading in SPI data and converting it to parallel
      */
     localparam SPI_DATA_BITS = 16;
-    reg [SPI_DATA_BITS-1:0] spi_parallel_out;
+    wire [SPI_DATA_BITS-1:0] spi_parallel_out;
     shift_register #(
         WIDTH = SPI_DATA_BITS
     ) spi_serial_in_parallel_out (
         .clk_i(clk_i),
         .rst_i(rst_i),
-        .advance_i(),
-        .bit_i(),
-        .value_o()
+        .advance_i(sclk_rising_edge),
+        .bit_i(sdata_i),
+        .value_o(spi_parallel_out)
     );
 
     /*
      * Sampling state machine
      */
     localparam
-        WAIT          = 3'h0,
-        CHIP_SELECT   = 3'h1,
-        READ_SAMPLE   = 3'h2,
-        BE_QUIET      = 3'h3,
-        SAMPLE_STROBE = 3'h4;
-    reg [2:0] state, next_state;
+        WAIT          = 5'b00001,
+        CHIP_SELECT   = 5'b00010,
+        READ_SAMPLE   = 5'b00100,
+        BE_QUIET      = 5'b01000,
+        SAMPLE_STROBE = 5'b10000;
+    reg [4:0] state, next_state;
 
     // State register
     initial state = WAIT;
@@ -95,20 +127,35 @@ module ad7476a_interface #(
                 if (request_i)
                     next_state = CHIP_SELECT;
             CHIP_SELECT:
-
+                if (timer_done)
+                    next_state = READ_SAMPLE;
             READ_SAMPLE:
+                if (got_16_sclk_falling_edges)
+                    next_state = BE_QUIET;
             BE_QUIET:
+                if (timer_done)
+                    next_state = SAMPLE_STROBE;
             SAMPLE_STROBE:
+                if (request_i)
+                    next_state = CHIP_SELECT;
+                else
+                    next_state = WAIT;
         endcase
         /* verilator lint_on CASEINCOMPLETE */
     end
 
     // State outputs
     always @(*) begin
+        cs_n_o = 1;
+        enable_sclk = 0;
+        start_timer = 0;
+        timer_count = T2;
         /* verilator lint_off CASEINCOMPLETE */
         case (state)
             WAIT:
+                start_timer = request_i; // TODO: finish this
             CHIP_SELECT:
+                cs_n_o = 0;
             READ_SAMPLE:
             BE_QUIET:
             SAMPLE_STROBE:
